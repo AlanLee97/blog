009a92169d04cf00eae4e529270f88e5a8c8e2a1
李步官 <42601044+AlanLee97@users.noreply.github.com>
2020-01-21 17:04:54 +0800

    add content

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,9 +4,17 @@ module.exports = {
     themeConfig: {
       type:'blog',
       authorAvatar: '/AlanLee.jpg',
-      sidebar: [
-        ['/', '首页'],
-        
-      ]
+      //导航栏
+      nav: [
+        { text: '主页', link: '/' },
+        { text: '服务器', link: '/server/' },
+        { text: '前端', link: '/frontend/' },
+        { text: '后端', link: '/backend/' },
+        { text: '移动端', link: '/mobileend/' },
+      ],
+      //侧边栏
+      sidebar: 'auto',
+      //显示所有标题链接
+      displayAllHeaders: true, // 默认值：false
     }
   }
--- docs/.vuepress/dist/assets/js/11.fcd8323e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{178:function(n,w,o){}}]);
